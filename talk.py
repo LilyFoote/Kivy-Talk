ccfd060b84c37d918acc1539477115d541bbf773
Ian Foote <ian@feete.org>
2013-07-07 15:53:46 +0100

    Add more kv language slides.
    Tweak kivy catalog's Container widget for presenting kv language
    examples.
    
    Expand Intro slide slightly.
    
    Use FadeTransition when changing slide.

--- talk.py
+++ talk.py
@@ -3,6 +3,10 @@ kivy.require('1.7.1')
 
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.factory import Factory
+from kivy.lang import Builder, Parser, ParserException
+from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
 
 class Slide(Screen):
@@ -28,6 +32,27 @@ class Slide(Screen):
         else:
             return False
         return True
+
+class Container(BoxLayout):
+    kv = StringProperty('')
+
+    def __init__(self, **kwargs):
+        super(Container, self).__init__(**kwargs)
+
+    def on_kv(self, instance, value):
+        try:
+            parser = Parser(content=value.encode('utf-8'))
+        except (SyntaxError, ParserException):
+            return
+
+        try:
+            widget = Factory.get(parser.root.name)()
+        except FactoryException:
+            return
+
+        Builder._apply_rule(widget, parser.root, parser.root)
+        self.clear_widgets()
+        self.add_widget(widget)
 
 class TalkApp(App):
     pass
